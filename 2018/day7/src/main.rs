293e54999a30e3f99ed12971cd13459ea250ee7e
runarer <95697611+runarer@users.noreply.github.com>
2026-08-25 10:49:59 +0200

    Parses file

--- 2018/day7/src/main.rs
+++ 2018/day7/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <input_file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let content = std::fs::read_to_string(&args[1])?;
+
+    let steps: Vec<(char, char)> = content.lines().map(|line| parse_step(line)).collect();
+
+    for step in steps {
+        println!("({},{})", step.0, step.1);
+    }
+
+    Ok(())
+}
+
+fn parse_step(line: &str) -> (char, char) {
+    let point = line.chars().nth(5).unwrap_or('0');
+    let depend_on = line.chars().nth(36).unwrap_or('0');
+    return (point, depend_on);
 }
