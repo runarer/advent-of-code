099e715e0fa97696386406657c4f7e58450be4b4
runarer <95697611+runarer@users.noreply.github.com>
2026-08-27 11:13:59 +0200

    Solves part 2 day 7 2018

--- 2018/day7/src/main.rs
+++ 2018/day7/src/main.rs
@@ -15,13 +15,12 @@ fn main() -> io::Result<()> {
 
     let graph = build_graph(steps);
 
-    for (node, nodes) in &graph {
-        println!("{},{:?}", node, nodes);
-    }
-
     let order = find_order(&graph);
 
+    let seconds_lapsed = find_time_with_five_workers(&graph);
+
     println!("Part 1: {}", order);
+    println!("Part 2: {}", seconds_lapsed);
 
     Ok(())
 }
@@ -97,7 +96,74 @@ fn find_empty(graph: &HashMap<char, Vec<char>>) -> Vec<char> {
     empty_lists
 }
 
-/*
+fn find_time_with_five_workers(in_graph: &HashMap<char, Vec<char>>) -> usize {
+    let mut graph = in_graph.clone();
+
+    let mut workers = vec![
+        (0, None::<char>),
+        (0, None::<char>),
+        (0, None::<char>),
+        (0, None::<char>),
+        (0, None::<char>),
+    ];
+
+    let mut total_time = 0;
+    // There's more work to be done or are workers working or
+    while graph.len() > 0 || workers.iter().any(|(sec, _)| *sec > 0) {
+        // let all working workers work
+        for (sec, _) in workers.iter_mut() {
+            if *sec > 0 {
+                *sec -= 1;
+            }
+        }
+
+        // Find work that is done and remove it from the dependecies
+        for (sec, node) in workers.iter_mut() {
+            if *sec == 0 && node.is_some() {
+                let just_done = node.unwrap();
+
+                for (_, dependensies) in graph.iter_mut() {
+                    if let Some(index) = dependensies.iter().position(|&x| x == just_done) {
+                        dependensies.swap_remove(index);
+                    }
+                }
+                // Free worker
+                *node = None;
+            }
+        }
+
+        // Find new work
+        let mut empty_lists = find_empty(&graph);
+        empty_lists.sort();
+
+        // Assign new work to workers
+        while empty_lists.len() > 0 && workers.iter().any(|(_, node)| node.is_none()) {
+            let label = empty_lists[0];
+
+            for (sec, node) in workers.iter_mut() {
+                if node.is_none() {
+                    // Assign work
+                    *node = Some(label);
+                    *sec = 60 + (label as u8) - 64; // 65 is the value of 'A' so using 64 to make it 1
+
+                    // Remove work from graph
+                    graph.remove(&label);
+
+                    // work is assigned, avoid new assignments
+                    break;
+                }
+            }
+
+            empty_lists.swap_remove(0);
+        }
+
+        total_time += 1;
+    }
+
+    total_time - 1 // remove last lap before returning
+}
+
+/* NOTES
     For del 2 trenger vi en loop for work som ticker pr sec. Vi må ha en oversikt over arbeidere.
     Kan være en array på 5 med antall sekunder igjen for arbeidet og label for node.
 
@@ -115,9 +181,5 @@ fn find_empty(graph: &HashMap<char, Vec<char>>) -> Vec<char> {
         hvis arbeider har node og er null:
             fjern node fra dependensies.
 
-
-
-
-
-
+    Del 2 ble implementer i en litt annen rekkefølge.
 */
