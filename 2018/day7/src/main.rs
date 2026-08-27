bde2139c53503f7d4c0be7d05c45a62c7e3b0796
runarer <95697611+runarer@users.noreply.github.com>
2026-08-27 09:30:31 +0200

    Solves part 1 day 7 2018

--- 2018/day7/src/main.rs
+++ 2018/day7/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -12,15 +13,87 @@ fn main() -> io::Result<()> {
 
     let steps: Vec<(char, char)> = content.lines().map(|line| parse_step(line)).collect();
 
-    for step in steps {
-        println!("({},{})", step.0, step.1);
+    let graph = build_graph(steps);
+
+    for (node, nodes) in &graph {
+        println!("{},{:?}", node, nodes);
     }
+
+    let order = find_order(&graph);
+
+    println!("Part 1: {}", order);
 
     Ok(())
 }
 
 fn parse_step(line: &str) -> (char, char) {
-    let point = line.chars().nth(5).unwrap_or('0');
-    let depend_on = line.chars().nth(36).unwrap_or('0');
+    let point = line.chars().nth(36).unwrap_or('0');
+    let depend_on = line.chars().nth(5).unwrap_or('0');
     return (point, depend_on);
+}
+
+fn build_graph(steps: Vec<(char, char)>) -> HashMap<char, Vec<char>> {
+    let mut graph: HashMap<char, Vec<char>> = HashMap::new();
+
+    for (step, depend_on) in &steps {
+        if !graph.contains_key(step) {
+            graph.insert(*step, Vec::new());
+        }
+        graph.get_mut(step).unwrap().push(*depend_on);
+    }
+
+    let mut missing_nodes: Vec<char> = Vec::new();
+    // Find missing nodes
+    for (_, depend) in &graph {
+        for d in depend {
+            if !graph.contains_key(d) {
+                missing_nodes.push(*d);
+            }
+        }
+    }
+
+    for node in &missing_nodes {
+        graph.insert(*node, Vec::new());
+    }
+
+    graph
+}
+
+fn find_order(in_graph: &HashMap<char, Vec<char>>) -> String {
+    let mut graph = in_graph.clone();
+    let mut order: String = String::new();
+
+    while graph.len() > 0 {
+        // find first (sorted)
+        let mut empty_lists = find_empty(&graph);
+        println!("{:?}", empty_lists);
+        empty_lists.sort();
+        let last_added = empty_lists[0];
+
+        // add it to order
+        order.push(last_added);
+
+        // Remove it from graph
+        graph.remove(&last_added);
+
+        for (_, dependensies) in graph.iter_mut() {
+            if dependensies.contains(&last_added) {
+                dependensies.retain(|&n| n != last_added);
+            }
+        }
+    }
+
+    order
+}
+
+fn find_empty(graph: &HashMap<char, Vec<char>>) -> Vec<char> {
+    let mut empty_lists: Vec<char> = Vec::new();
+
+    for (node, dependensies) in graph {
+        if dependensies.len() == 0 {
+            empty_lists.push(*node);
+        }
+    }
+
+    empty_lists
 }
